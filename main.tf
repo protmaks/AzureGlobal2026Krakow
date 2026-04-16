8d5d4b83dbc137743df1300a08aa14358399dc68
protmaks <9646520@gmail.com>
2026-04-16 20:36:06 +0200

    7

--- main.tf
+++ main.tf
@@ -8,8 +8,15 @@ terraform {
 }
 provider "azurerm" {
   features {}
-  
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "subscription_id" {}
 
 terraform {
   backend "azurerm" {
