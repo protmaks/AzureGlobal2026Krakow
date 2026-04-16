8d12764732934cf1cace276f69e8f7a1ab4ca9d9
protmaks <9646520@gmail.com>
2026-04-16 19:53:23 +0200

    fix

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.1.0"
+      version = "~> 4.0"
     }
   }
 }
@@ -16,5 +16,18 @@ terraform {
     storage_account_name = "stprotmaks"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+  }
+}
+
+module "keyvault" {
+  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=keyvault/v1.0.0"
+  keyvault_name = "protmaks"
+  resource_group = {
+    name     = "rg-user11"
+    location = "northeurope"
+  }
+  network_acls = {
+    bypass = "AzureServices"
+    default_action = "Deny"
   }
 }
